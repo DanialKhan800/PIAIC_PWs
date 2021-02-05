0fafef85ffab5949f44305e3f4db8261c03c34e7
Muhammad Danial Khan <72875993+DanialKhan800@users.noreply.github.com>
2021-02-05 21:55:17 +0500

    Delete AssignmentJavaScript/assignment_PIAIC135398 directory

--- AssignmentJavaScript/assignment_PIAIC135398/Task1/task1.js
+++
@@ -1,5 +0,0 @@
-var arr = ["SSC","HSC","BSC","BS","BCOM","MS","M.PHIL","PhD" ];
-for (var i =0 ; i<arr.length; i++)
-{
-    document.write("<h1>" +(i+1)+")"+arr[i]+ "</h1>")
-}
--- AssignmentJavaScript/assignment_PIAIC135398/Task6/task6.js
+++
@@ -1,8 +0,0 @@
-let aCities = ['Karachi', 'Lahore', 'Islamabad','Faisalabad'];
-let o = ["st","nd","rd","th"];
-
-for (let i=0 ; i<aCities.length ; i++)
-{
-    document.write("<h2>" + (i+1) + o[i] + " choice is " + aCities[i]+ "</h2>");
-
-}
